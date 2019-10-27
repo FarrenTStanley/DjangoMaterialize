d3252715973cd5f6ec5f89ec002dcb373b1c9b74
FarrenStanley <farren.t.stanley@gmail.com>
2019-10-26 23:17:45 -0400

    Initial commit

---
+++ mysite/main/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Tutorial, TutorialCategory, TutorialSeries
+
+
+def single_slug(request, single_slug):
+    categories = [c.category_slug for c in TutorialCategory.objects.all()]
+    if single_slug in categories:
+        matching_series = TutorialSeries.objects.filter(tutorial_category__category_slug=single_slug)
+
+        series_urls = {}
+        for m in matching_series.all():
+            part_one = Tutorial.objects.filter(tutorial_series__tutorial_series=m.tutorial_series)\
+                .earliest("tutorial_published")
+            series_urls[m] = part_one.tutorial_slug
+
+        return render(request,
+                      "main/category.html",
+                      {"part_ones": series_urls})
+
+    tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
+
+    if single_slug in tutorials:
+        this_tutorial = Tutorial.objects.get(tutorial_slug=single_slug)
+
+        return render(request=request,
+                      template_name='main/tutorial.html',
+                      context={"tutorial": this_tutorial})
+
+    return HttpResponse(f"'{single_slug}' does not correspond to anything we know of!")
+
+
+# Create your views here.
+def homepage(request):
+    return render(request=request,
+                  template_name='main/categories.html',
+                  context={"categories":TutorialCategory.objects.all})
